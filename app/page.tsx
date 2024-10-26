c913b07a647182756ae9f0268f00b212f9c81178
deveshtuteja <deveshtuteja17@gmail.com>
2024-10-27 02:58:02 +0530

    page setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,8 @@
 export default function Page() {
   return (
+    <div className="h-[100vh] w-full flex items-center justify-center text-center">
       <h1 className="text-5xl text-white">Figma Clone</h1>
+    </div>
+      
   );
 }
